8eab7ceba714b82c8c778851153f9e7a405f0388
Khyra <khyrak@gmail.com>
2022-09-14 14:45:21 -0300

    fix lint 3

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,7 +1,5 @@
 db.produtos.find({
-  $and: [ 
+  $and: [
     { vendidos: { $ne: 50 } },
-    { tags: { $exists: false } },
-  ],
-}, { _id: 0, nome: 1, vendidos: 1 },
-);
+    { tags: { $exists: false } }] },
+    { nome: 1, vendidos: 1, _id: 0 });
