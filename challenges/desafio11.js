62a35ace5ee171982c4f6b9777b387cc0c892f58
Khyra <khyrak@gmail.com>
2022-09-14 17:49:31 -0300

    req 11 - fix lint

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,5 +1,5 @@
 db.produtos.find({ 
   $and: [
-    { nome: { $not: { $eq: "Big Mac"} }},
-    { nome: { $not: { $eq: "McChicken"} }}] },
+    { nome: { $not: { $eq: "Big Mac" } } },
+    { nome: { $not: { $eq: "McChicken" } } }] },
 { _id: 0, nome: 1, curtidas: 1, vendidos: 1 });
