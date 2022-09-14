12efa167844f7b172baffb1c71cad7de38241412
Khyra <khyrak@gmail.com>
2022-09-14 15:29:34 -0300

    req 9, bora meu fi

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,9 +1,5 @@
-db.produtos.find({
-  valoresNutricionais:
-  {
-    $elemMatch: { 
-    tipo: "calorias",
-    quantidade: { $lt: 500 },
-    },
-  } },
-   { nome: 1 });
+db.produtos.find(
+  { valoresNutricionais: { 
+    $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } } 
+  }, { _id: 0, nome: 1 },
+);
