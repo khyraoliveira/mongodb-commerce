5624c44aed5e05ce9bf888232bdbd8e5fdc9713a
Khyra <khyrak@gmail.com>
2022-09-14 22:22:30 -0300

    fix lint

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -3,7 +3,6 @@ db.produtos.updateMany({
     { nome: "Big Mac" }, { nome: "Quarteirão com Queijo" },
   ],
  },
-  { $push: { ingredientes: "bacon" } },
- );
+  { $push: { ingredientes: "bacon" } });
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
