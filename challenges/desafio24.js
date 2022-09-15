daf193321cbe6893f597b317321ba386cfa149a2
Khyra <khyrak@gmail.com>
2022-09-14 23:58:35 -0300

    fix lint

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany({},
   { $push: { 
     valoresNutricionais: { 
-      $each: [], $sort: { percentual: -1 } 
+      $each: [], $sort: { percentual: -1 }, 
     },
    },
   });
--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -2,7 +2,6 @@ db.produtos.updateMany({ valoresNutricionais:
   { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } }, 
   { $push: { tags: { $each: ["muito sódio"] } } });
 
-
 db.produtos.find({}, 
   { 
     nome: 1,
